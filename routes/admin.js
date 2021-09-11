aed2424d3008454d5ef265f949e21e17d93c1c8f
Rodrigo Lucero <lucero.rodrigoar@gmail.com>
2021-09-11 19:21:32 -0300

    Implemented MVC

--- routes/admin.js
+++ routes/admin.js
@@ -2,19 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProduct);
 
-router.post('/add-product', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
+// /admin/add-product => POST
+router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;
-
