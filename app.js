f7ed6adb1d5534caad4fde27c1eafb7a64e0b52b
Rodrigo Lucero <lucero.rodrigoar@gmail.com>
2021-09-11 12:19:42 -0300

    Added filtering paths and helper function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,11 +9,11 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded());
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found </h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> AddProduct</button></form>');
+    res.sendFile(path.join(rootDir, '../', 'views', 'add-product.html'));
 });
 
 router.post('/product', (req, res) => {
